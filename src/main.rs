cc41848a04bb38f06272a8313f9e481692c63e80
Martin Feineis <martinfeineis@gmx.de>
2018-10-19 21:23:53 +0200

    HashMaps

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use rand::Rng;
 use std::cmp::Ordering;
 //use std::fmt::Display;
 use std::io;
+use std::collections::HashMap;
 
 use communicator::network;
 
@@ -206,6 +207,61 @@ fn main() {
     }
 
     // TODO: grapheme clusters via non std create
+
+    {
+        let mut scores = HashMap::new();
+        scores.insert(String::from("Blue"), 10);
+        scores.insert(String::from("Yellow"), 50);
+    }
+
+    {
+        let teams = vec![String::from("Blue"), String::from("Yellow")];
+        let initial_scores = vec![10, 50];
+
+        let _scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    }
+
+    {
+        let field_name = String::from("Favorite color");
+        let field_value = String::from("Blue");
+
+        let mut map = HashMap::new();
+        map.insert(field_name, field_value);
+        // field_name and field_value are invalid at this point!
+
+        // println!("field_name = {}", field_name); // Won't compile!
+        let team_name = String::from("Blue");
+        match map.get(&team_name) {
+            Some(_) => println!("Found!"),
+            None => println!("Not Found!"),
+        }
+
+        let mut scores = HashMap::new();
+        scores.insert(String::from("Blue"), 10);
+        scores.insert(String::from("Yellow"), 50);
+
+        for (key, value) in &scores {
+            println!("{} -> {}", key, value);
+        }
+
+        // Replace via insert and same key
+        scores.insert(String::from("Blue"), 11);
+
+        // Insert if not already present
+        scores.entry(String::from("Yellow")).or_insert(51);
+    }
+
+    {
+        let text = "hello world wonderful world";
+        let mut map = HashMap::new();
+
+        for word in text.split_whitespace() {
+            let count = map.entry(word).or_insert(0);
+            *count += 1;
+        }
+
+        println!("{:?}", map);
+    }
 
     let _no_dangling_reference = no_dangle();
 
